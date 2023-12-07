433f0caabb0b1ffb6ae9d7241324d7c0b2f29086
exteriorduke319 <yarnevictanner@gmail.com>
2023-12-07 09:39:50 -0500

    init

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,69 @@
 import java.util.Scanner;
-
+// Make it substring instead of checking entire password
 public class Main {
     public static void main(String[] args) {
         Scanner hov = new Scanner(System.in);
-
-        String password;
-        int testPassed = 0;
-        boolean doneCheck = true;
+        boolean doneCheck;
 
         do {
-            password = InputHelper.getNonZeroLenString(hov, "Please enter your password");
+            // Resets everything if the user enters another password
+            int testPassed = 5;
+            boolean caps = false;
+            boolean lower = false;
+            boolean num = false;
+            boolean specialChar = false;
+            String password = InputHelper.getNonZeroLenString(hov, "Please enter your password");
+            String current;
 
-            if (password.length() >= 12) {
-                testPassed++;
-            } else {
-                System.out.println("Your password should be at least 12 characters long");
+            // Determines if the password passes the tests or not
+            for (int i = 0; i < password.length(); i++) {
+                current = password.substring(i, i+1);
+
+                if(current.matches("[A-Z]")) {
+                    caps = true;
+                }
+
+                if(current.matches("[a-z]")) {
+                    lower = true;
+                }
+
+                if(current.matches("[0-9]")) {
+                    num = true;
+                }
+
+                if(current.matches("[!@#$%^&*]")) {
+                    specialChar = true;
+                }
             }
 
-            if(password.matches("[A-Z]")) {
-                testPassed++;
-            } else {
+            // Overly complicated section of code that takes away from tests passed if a test isn't passed
+            if(!caps) {
                 System.out.println("Your password should contain at least 1 capital letter.");
+                testPassed--;
             }
 
-            if(password.matches("[a-z]")) {
-                testPassed++;
-            } else {
+            if (!lower) {
                 System.out.println("Your password should contain at least 1 lowercase letter.");
+                testPassed--;
             }
 
-            if(password.matches("[0-9]")) {
-                testPassed++;
-            } else {
+            if (!num){
                 System.out.println("Your password should contain at least 1 number.");
+                testPassed--;
             }
 
-            if(password.matches("[!@#$%^&*]")) {
-                testPassed++;
-            } else {
+            if (!specialChar){
                 System.out.println("Your password should contain at least 1 special character.");
+                testPassed--;
             }
 
+            if (password.length() < 12) {
+                System.out.println("Your password should be at least 12 characters long");
+                testPassed--;
+            }
+
+            // Tells the user the password strength and how many tests it passed
+            System.out.println();
             System.out.println("Your password passed " + testPassed + "/5 tests.");
             if (testPassed >= 4) {
                 System.out.println("Your password's strength is strong");
@@ -50,7 +73,9 @@ public class Main {
                 System.out.println("Your password's strength is weak.");
             }
 
+            System.out.println();
             doneCheck = InputHelper.getYNConfirm(hov, "Would you like to check another password?");
+
         } while (doneCheck);
 
     }
